9fb79a660bd36058884aae665a9af38d90b8d7d3
vincentruan <rzw0813@gmail.com>
2025-06-11 17:52:19 +0800

    feat: add mcp webflux

--- omnilink-bridge/omnilink-bridge-javaseeker/build.gradle.kts
+++ omnilink-bridge/omnilink-bridge-javaseeker/build.gradle.kts
@@ -15,11 +15,12 @@ application {
 dependencies {
     // api(libs.spring.ai.mcp.server.spring.boot.starter) // Removed MCP Starter
 //    implementation(platform(libs.spring.ai.bom)) // Explicitly apply Spring AI BOM for troubleshooting
-    api(libs.org.springframework.spring.web)
+//    api(libs.org.springframework.spring.web)
 //    implementation(libs.org.springframework.boot.spring.boot.starter)
 //    implementation(libs.org.springframework.boot.spring.boot.starter.web) // For RestClient and Jackson
     // implementation("org.springframework.ai:spring-ai-core:1.0.0") // Removed direct spring-ai-core
-    implementation(libs.spring.ai.mcp.server) // Use corrected OpenAI starter alias
+//    implementation(libs.spring.ai.mcp.server) // Use corrected OpenAI starter alias
+    implementation(libs.spring.ai.mcp.server.webflux)
     implementation(libs.spring.boot.starter.logging)
     implementation(libs.spring.boot.starter.actuator)
     // implementation("io.modelcontextprotocol:modelcontextprotocol-client")
